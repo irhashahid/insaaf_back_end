0405db3446bc1ded653e886a898d21fc5c6b5912
Anas Shahid <irhashahid7890@gmail.com>
2026-04-26 16:13:36 +0500

    MVC crrction

--- routes/lawyerRoutes.js
+++ routes/lawyerRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const authMiddleware = require("../middlewares/authMiddleware");
+const authMiddleware = require("../middleware/authMiddleware");
 const {
   index, show, create, update, remove, updateStatus, approved,
 } = require("../controllers/lawyerController");
