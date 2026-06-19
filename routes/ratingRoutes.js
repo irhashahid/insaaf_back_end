624393c513f063594466027b74c853816844a377
Anas Shahid <irhashahid7890@gmail.com>
2026-06-19 11:26:11 +0500

    API/s tested

--- routes/ratingRoutes.js
+++ routes/ratingRoutes.js
@@ -5,7 +5,7 @@ const { byLawyer, create, remove } = require("../controllers/ratingController");
 
 // specific routes BEFORE /:id
 router.get("/lawyer/:lawyerId", byLawyer);              // GET /ratings/lawyer/3
-router.post("/", authMiddleware, create);               // POST /ratings
-router.delete("/:id", authMiddleware, remove);          // DELETE /ratings/1
+router.post("/", authMiddleware, create);               // POST /ratings...........api for client... rating bs postman pe add krni nd not in db 
+router.delete("/:id", authMiddleware, remove);          // DELETE /ratings/1......id mein jo rating table ki id hai woh hit krni na k client kii id 
 
 module.exports = router;
